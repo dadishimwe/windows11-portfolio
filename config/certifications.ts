dc0ae30bb534168d1cf1c8a812f07d1fa71d091d
Dadi Ishimwe <dadishimwe29@gmail.com>
2026-06-23 22:54:00 +0200

    fix syntax

--- config/certifications.ts
+++ config/certifications.ts
@@ -25,35 +25,35 @@ export const certifications: Certification[] = [
 		issuer: 'Fortinet',
 		dateModified: '4/7/2026',
 		credlyUrl: 'https://www.credly.com/badges/129f798c-f2bd-4723-9970-e5410a5ae726/public_url',
-	}
+	},
 	{
 		id: 'fortinet-cfc',
 		name: 'Fortinet Certified Fundamentals Cybersecurity',
 		issuer: 'Fortinet',
 		dateModified: '4/18/2026',
 		credlyUrl: 'https://www.credly.com/badges/57cbfbec-00b2-4304-ab44-e47210dee7ea/public_url',
-	}
+	},
 	{
 		id: 'fortinet-nse-2',
 		name: 'Technical Introduction to Cybersecurity 3.0',
 		issuer: 'Fortinet',
 		dateModified: '4/19/2026',
 		credlyUrl: 'https://www.credly.com/badges/61949431-879d-4fe8-b4b6-d5b44c24194f/public_url',
-	}
+	},
 	{
 		id: 'fortinet-cac',
 		name: 'Fortinet Certified Associate Cybersecurity',
 		issuer: 'Fortinet',
 		dateModified: '5/17/2026',
 		credlyUrl: 'https://www.credly.com/badges/7439fe65-0fa1-4e93-8bff-130ee53fae5d/public_url',
-	}
+	},
 	{
 		id: 'fortinet-nse-4',
 		name: 'Fortinet FortiGate 7.6 Operator',
 		issuer: 'Fortinet',
 		dateModified: '5/17/2026',
 		credlyUrl: 'https://www.credly.com/badges/d3b2a36f-d54f-42a0-b4db-27dd67d613a3/public_url',
-	}
+	},
 ];
 
 export function getCertificationHref(cert: Certification): string | undefined {
